4ebceaf44d2620cf6eb7617c0e9791911b34e378
Gwendall Esnault <gwendall.esnault@gmail.com>
2020-02-21 14:14:54 +0100

    default language value not set

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -3,6 +3,4 @@ import { appWithTranslation } from "./../i18n";
 
 const App = ({ Component, pageProps }) => <Component {...pageProps} />;
 
-const App2 = appWithTranslation(App);
-App2.getInitialProps = App.getInitialProps;
-export default App2;
+export default appWithTranslation(App);
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,22 @@
 import React from "react";
-import { appWithTranslation, useTranslation } from "./../i18n";
+import { i18n, useTranslation } from "./../i18n";
 
 const Page = () => {
   const { t } = useTranslation();
-  return <div>{t("hello.world")}</div>;
+  return (
+    <>
+      <div>{t("hello.world")}</div>
+      {["fr", "en"].map((locale, i) => (
+        <button
+          key={`locale.${i}`}
+          style={{ background: locale === i18n.language ? "red" : "none" }}
+          onClick={i18n.changeLanguage.bind(this, locale)}
+        >
+          {locale}
+        </button>
+      ))}
+    </>
+  );
 };
 
 export default Page;
